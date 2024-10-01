37c3261d3c233b592d52f18ba035dfdac94ac8b1
regression-test-executor <regression-test-executor@speakeasy.com>
2024-10-01 13:31:08 +0000

    openapi-generation@52c5f018c25a2a3803855842a59b3c4e61f8a6b0

--- Boltpay/SDK/Payments.cs
+++ Boltpay/SDK/Payments.cs
@@ -25,9 +25,9 @@ namespace Boltpay.SDK
         public SDKConfig SDKConfiguration { get; private set; }
         private const string _language = "csharp";
         private const string _sdkVersion = "0.2.1";
-        private const string _sdkGenVersion = "2.409.8";
+        private const string _sdkGenVersion = "2.428.4";
         private const string _openapiDocVersion = "3.2.1";
-        private const string _userAgent = "speakeasy-sdk/csharp 0.2.1 2.409.8 3.2.1 Boltpay.SDK";
+        private const string _userAgent = "speakeasy-sdk/csharp 0.2.1 2.428.4 3.2.1 Boltpay.SDK";
         private string _serverUrl = "";
         private ISpeakeasyHttpClient _client;
         private Func<Boltpay.SDK.Models.Components.Security>? _securitySource;
